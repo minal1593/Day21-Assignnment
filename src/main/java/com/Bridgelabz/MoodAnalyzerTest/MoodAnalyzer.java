0dae79e0247940c1e2eeb72527e8aa79a7a1c8d8
minal1593 <minalsalunkhe15@gmail.com>
2022-07-28 18:32:11 +0530

    Added : Program for MoodAnlyzer to inform user if entered invalid mood

--- src/main/java/com/Bridgelabz/MoodAnalyzerTest/MoodAnalyzer.java
+++ src/main/java/com/Bridgelabz/MoodAnalyzerTest/MoodAnalyzer.java
@@ -2,18 +2,28 @@ package com.Bridgelabz.MoodAnalyzerTest;
 
 public class MoodAnalyzer {
     private String message;
+
     public MoodAnalyzer(String message) {
         this.message = message;
     }
 
-    public String analyseMood() {
+    public String analyseMood(String message) throws MoodAnalysisException {
+        this.message = message;
+        return analyseMood();
+    }
+
+    public String analyseMood() throws MoodAnalysisException {
         try {
+            if (message.length() == 0)
+                throw new MoodAnalysisException(MoodAnalysisException.exceptionType.ENTERED_EMPTY, "Please enter Proper Mood");
             if (message.contains("Sad"))
                 return "SAD";
             else
                 return "HAPPY";
-        }catch (NullPointerException e){
-            return "HAPPY";
+        }
+        catch (NullPointerException e)
+        {
+            throw new MoodAnalysisException(MoodAnalysisException.exceptionType.ENTERED_NULL, "Please enter Proper Mood");
         }
     }
 }
