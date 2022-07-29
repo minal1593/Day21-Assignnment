73243d8375c40f0dc2e6cd806939b5f40f3e6d0f
minal1593 <minalsalunkhe15@gmail.com>
2022-07-29 19:27:27 +0530

    Added: Program for UserRegistration to validate Password(Should at least 1 numeric number  )

--- src/main/java/com/Bridgelabz/UserRegistration/Uc7_UserRegistration.java
+++ src/main/java/com/Bridgelabz/UserRegistration/Uc7_UserRegistration.java
@@ -2,13 +2,13 @@ package com.Bridgelabz.UserRegistration;
 
 import java.util.Scanner;
 
-public class Uc6_UserRegistration {
-    static String Password= "[A-Z+a-z\\d@$!%*?&]{8,}";
+public class Uc7_UserRegistration {
+    static String Password= "[A-Za-z\\d{1,}@$!%*?&]{8,}";
 
     public static void main(String args[])
     {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter the valid Password(Should have at least one Upper case) ");
+        System.out.println("Enter the valid Password(At least 1 Numeric number) ");
         String testName = scan.nextLine();
         if(testName.matches(Password))
         {
